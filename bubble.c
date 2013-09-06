dd828a8fbeea29228ea10a07636940483d804fbf
Fernando Pereira <fernandhenriqp@gmail.com>
2013-09-05 22:46:34 -0300

    including bubblesort

---
+++ bubble.c
@@ -0,0 +1,35 @@
+/* BubbleSort */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+
+
+int main(int argc, char** argv){
+  int i, buffer;
+  char flag;
+  int size = argc-1;
+  int* v = (int*)malloc(sizeof(int)*size);
+  
+  for(i=0; i<size; i++){
+    v[i] = atoi(argv[i+1]);
+  }
+  
+  do{
+    flag = 0;
+    for(i=1; i<size; i++){
+      if( v[i-1] > v[i] ){
+	SWAP(v[i-1],v[i],buffer);
+	flag=1;
+      }
+    }
+  }while(flag);
+  
+  for(i=0; i<size; i++){
+    printf("%d,", v[i]);
+  }
+
+  printf("\b\n");
+  return 0;
+}
---
+++ sort.h
@@ -0,0 +1,12 @@
+/* sort.h: Define some functions and constants for sorting */
+/* By Fernando Pereira */
+
+
+#ifndef SORT_H
+#define SORT_H
+
+
+#define SWAP(x,y,buffer) do{ (buffer)=(x); (x)=(y); (y)=(buffer); }while(0); 
+
+
+#endif
