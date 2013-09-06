f76aa8061ee3930563d118a4b5f678bd7858c498
Fernando Pereira <fernandhenriqp@gmail.com>
2013-09-06 00:27:20 -0300

    conserto do insertionsort..

--- insertion.c
+++ insertion.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include "sort.h"
 
-//#define DEBUG 0
+#define DEBUG 0
 
 int main(int argc, char** argv){
-  int i, j, buffer;
-  int cand; // candidate
+  int i, j, k, buffer;
+  int aux; // candidate
   char flag;
   int size = argc-1;
   int* v = (int*)malloc(sizeof(int)*size);
@@ -17,14 +17,17 @@ int main(int argc, char** argv){
     v[i] = atoi(argv[i+1]);
   }
   
-  for(i=0; i<size; i++){
-    cand = i;
-    for(j=i; j<size; j++){
-      if( v[j] < v[cand] ){
-	cand = j;
+  for(i=1; i<size; i++){  // Mod: eu tinha feito selection... oops
+    for(j=0; j<i; j++){
+      if( v[i] < v[j] ){
+	aux = v[i];
+	for( k=i; k>j; k--){
+	  v[k] = v[k-1];
+	}
+	v[j] = aux;
+	break;
       }
     }
-    SWAP(v[i], v[cand], buffer);
   }
 
 #ifdef DEBUG
