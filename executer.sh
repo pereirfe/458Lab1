3c11298fa166ad3057391b8543a12b4e64ab8e5a
Fernando Pereira <fernandhenriqp@gmail.com>
2013-09-10 22:25:35 -0300

    Arrumando...

--- executer.sh
+++ executer.sh
@@ -6,6 +6,7 @@ if [ $# -ne 6 ]
 then
     echo 'Uso: bash executer $algo $step $end $times $input $output'
 else
+    alg=$1
     p=$2
     top=$3
     tot=$4
@@ -19,7 +20,7 @@ else
 	x=$p
 	while [ $x -ne $top ]
 	do
-	    `bash tester.sh $1 $x $inp $out`
+	    `bash tester.sh $alg $x $inp $out`
 	    echo "Im at $x x $i"
 	    x=`echo "$x + $p" | bc` 
 	done
